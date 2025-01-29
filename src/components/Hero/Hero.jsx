6c8bac1b1a613ee5210410b5d5c8df71ab676d2e
shivamy009 <shivam0342005@gmail.com>
2025-01-29 10:10:21 +0530

    hello

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,7 +7,7 @@ export const Hero = () => {
   return (
     <section className={styles.container}>
       <div className={styles.content}>
-        <h1 className={styles.title}>Hi, I'm Shivam</h1>
+        <h1 className={styles.title}>Hi, I'm Shivam Yadav</h1>
         <p className={styles.description}>
           I'm a full-stack developer with 3 years of experience using MERN stack. Reach out if you'd like to learn more!
         </p>
