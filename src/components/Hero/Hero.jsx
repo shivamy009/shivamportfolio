c0882fd1a209d1f3efacf1668f0c7d8211167445
shivamy009 <shivam0342005@gmail.com>
2025-02-01 00:52:32 +0530

    hui

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import styles from "./Hero.module.css";
 import { getImageUrl } from "../../utils";
+import { MdOutlineFileDownload } from "react-icons/md";
 
 export const Hero = () => {
   return (
@@ -11,9 +12,21 @@ export const Hero = () => {
         <p className={styles.description}>
           I'm a full-stack developer with 3 years of experience using MERN stack. Reach out if you'd like to learn more!
         </p>
+        <div className={styles.contactResume}>
         <a href="mailto:myemail@email.com" className={styles.contactBtn}>
           Contact Me
         </a>
+
+        <a href="../../Shivam-Resume.pdf" className={styles.aDowntag} download="Shivam_Yadav_Resume.pdf">
+      <div className={styles.resumeBtn}>
+        <div>Resume</div>
+        <div className={styles.downBtn}>
+          <MdOutlineFileDownload />
+        </div>
+      </div>
+    </a>
+
+        </div>
       </div>
       <img
         src={getImageUrl("hero/homeImg.png")}
